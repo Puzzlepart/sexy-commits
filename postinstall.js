198be59e29bbfa9b3d59ad25993e76f311e90d8b
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-26 15:27:48 +0100

    0.1.15

--- postinstall.js
+++ postinstall.js
@@ -1,11 +1,11 @@
 "use strict";
 const fs = require('fs')
 const path = require('path')
-const packageJson = require('../../package.json')
+const packageJson = require('./package.json')
 
 packageJson.scripts['commit'] = 'sexy-commits'
 
 fs.writeFile(
-    path.resolve(__dirname, '../../package.json'),
+    path.resolve(__dirname, './package.json'),
     JSON.stringify(packageJson, null, 2)
 )
