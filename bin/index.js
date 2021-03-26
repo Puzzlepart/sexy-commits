ab7b6ac4f5af93e02e77ea9454a459b00afcd052
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-26 14:58:30 +0100

    0.1.7

--- bin/index.js
+++ bin/index.js
@@ -2,22 +2,43 @@
 "use strict";
 const inquirer = require('inquirer')
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
-const path = require('path')
 const util = require('util')
 const { cyan, white, red } = require('chalk')
 const log = console.log
-const { gitmoji } = require('../../../package.json')
+const fs = require('fs')
+const packageJson = require('../../../package.json')
+const { gitmoji: default_config } = require('./default_config.json')
 const child_process = require('child_process')
 const exec = util.promisify(child_process.exec)
+const writeFileAsync = util.promisify(fs.writeFile)
 
 async function commit_changes() {
+    let { gitmoji } = packageJson
+    if (!gitmoji) {
+        const { add_defaults } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'add_defaults',
+                message: `You don\'t have ${cyan(gitmoji)} config in your package.json. Add defaults?`,
+                default: true
+            }
+        ])
+        if (!add_defaults) {
+            process.exit(0)
+        }
+        await writeFileAsync('../../../package.json', JSON.stringify({
+            ...packageJson,
+            gitmoji: default_config
+        }, null, 2))
+        gitmoji = default_config
+    }
     const input = await inquirer.prompt([
         {
             type: 'autocomplete',
             name: 'commit_prefix',
             message: 'What did you do?',
             source: async (_a, input) => {
-                const types = Object.keys(gitmoji)
+                const types = Object.keys(packageJson.gitmoji)
                 return types
                     .filter(
                         (type) =>
